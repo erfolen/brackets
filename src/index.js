b4591259441855c35f92ed946556575b206bed69
erfolen <dlababy@mail.ru>
2020-10-19 22:33:30 +0300

    init: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let b = [];
+  let c = [];
+  let res = [];
+  
+  bracketsConfig.forEach (function (item) {
+      b.push(item[0]);
+      c.push(item[1]);
+  });  
+  for (let i = 0; i < str.length; i++) {
+    let d = b.indexOf(res[res.length - 1]);
+    (d > -1) ? d : d = false;
+    if (c.indexOf(str[i]) === d) {
+        res.pop();
+      } else if (b.includes(str[i])) {
+        res.push(str[i]);
+      } else {
+        return false;
+      }
+  }
+  return res.length === 0;
 }
